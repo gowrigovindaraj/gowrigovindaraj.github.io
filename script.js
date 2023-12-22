2351bdbf22dba9f49fb7fd49c8f5a26f3f96194d
gowrigovindaraj <80766920+gowrigovindaraj@users.noreply.github.com>
2023-12-22 19:03:56 +0530

    day 2 update

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Add smooth scrolling to navbar links
+  const navLinks = document.querySelectorAll('.nav-list a');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        window.scrollTo({
+          top: targetSection.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+});
